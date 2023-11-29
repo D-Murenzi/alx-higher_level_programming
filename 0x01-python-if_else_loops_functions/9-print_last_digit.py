34eb3dbb8e91099e1449e8532d310477c59f078f
D-Murenzi <murenzidavid01@gmail.com>
2023-11-29 22:19:15 +0200

    using modules of 10 to print the last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     # prints a last digit of a number
-    if isinstance(number, int):
-        a = str(number)
-        print(a[-1], end="")
-        return int(a[-1])
-    else:
-        return
-    
+    a = number %  10
+    print(a)
+    return a
