70b90df2a44b702f8c674f8edaf043b456f1a013
D-Murenzi <murenzidavid01@gmail.com>
2023-11-30 01:22:03 +0200

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     # function that capitalizes a string
+    str1 = ""
     for a in str:
         if ord(a) >= 97 and ord(a) <= 122:
             # converts a small letters to capital
-            print("{:s}".format(chr(ord(a)-32)), end="")
+            str1 = str1 + chr(ord(a)-32)
         else:
             # prints any other letter as it is
-            print("{:}".format(a), end="")
+            str1 = str1 + a
+    print("{:}".format(str1))
