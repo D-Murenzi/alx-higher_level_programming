0b038b901d1698b5a79abf8195b28a3d1b52efb2
D-Murenzi <murenzidavid01@gmail.com>
2023-12-04 22:11:20 +0200

    data structures repo, and task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    # this function prints all the elements of a list given
+    for a in my_list:
+        c = "{:d}".format(a)
+        print(c)
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+this is a repository containing my work on python data structures
+it is for learning purpose and for any other pupose reach out to me
+author: D-Murenzi
