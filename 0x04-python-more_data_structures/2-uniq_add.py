313d665028bc4e63c38e6d34416b76e2b31529f9
D-Murenzi <murenzidavid01@gmail.com>
2023-12-07 00:37:33 +0200

    task 2 debugged

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,7 @@
 def uniq_add(my_list=[]):
     # adds the unique integers in a list
     my_list.sort()
-    new_list = [my_list[0]]
-    for a in my_list:
-        if a != new_list[-1]:
-            new_list = new_list + [a]
+    new_list = set(my_list)
     add = 0
     for b in new_list:
         add = add + b
