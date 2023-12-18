7a61c5e4fd6849279c12439154ec1938693cb4c4
D-Murenzi <murenzidavid01@gmail.com>
2023-12-18 16:46:42 +0200

    task1, readme, and whole repo

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+
+    b = 0
+    for a in range(x):
+        try:
+            print(my_list[a], end="")
+            b = b + 1
+        except IndexError:
+            print()
+            return b
+    print()
+    return b
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,4 @@
+This directory is about exceptions and their
+handling.
+it for learning purpose, for any other purpose concert me
+Author: D-Murenzi.
